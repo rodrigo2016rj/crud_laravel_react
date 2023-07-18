1573172a51044eeda693a80bf85c6428f1da7487
Rodrigo Diniz da Silva <rodrigo_diniz_da_silva@yahoo.com>
2023-07-18 15:35:43 -0300

    Melhorei a configuração deste sistema, adicionei o channel deprecation para que avisos relacionados aos códigos que não funcionarão em versões futuras possam ser escritos em um arquivo de nome deprecation_warnings.log da pasta logs que fica dentro da pasta storage. A configuração do arquivo .env LOG_DEPRECATIONS_CHANNEL=null continua servindo para omitir estes avisos, enquanto que LOG_DEPRECATIONS_CHANNEL=deprecation ativará o novo canal de log. Atualizei o arquivo .env.example (modelo para o arquivo .env) para que o valor de LOG_DEPRECATIONS_CHANNEL seja o canal deprecation. Importante: Sempre que mudar o valor de LOG_DEPRECATIONS_CHANNEL do arquivo .env (inclusive a mudança sugerida por este commit), é necessário o uso do comando "php artisan config:cache" para refazer o cache para as configurações. Se for preferido não utilizar cache, o comando "php artisan config:clear" pode ser usado para desfazer o cache para as configurações.

--- crud_laravel_react/config/logging.php
+++ crud_laravel_react/config/logging.php
@@ -5,8 +5,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
 return [
-
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Default Log Channel
     |--------------------------------------------------------------------------
@@ -15,11 +14,10 @@ return [
     | messages to the logs. The name specified in this option should match
     | one of the channels defined in the "channels" configuration array.
     |
-    */
+   */
+  'default' => env('LOG_CHANNEL', 'stack'),
 
-    'default' => env('LOG_CHANNEL', 'stack'),
-
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Deprecations Log Channel
     |--------------------------------------------------------------------------
@@ -28,14 +26,13 @@ return [
     | regarding deprecated PHP and library features. This allows you to get
     | your application ready for upcoming major versions of dependencies.
     |
-    */
+   */
+  'deprecations' => [
+    'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
+    'trace' => false
+  ],
 
-    'deprecations' => [
-        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
-        'trace' => false,
-    ],
-
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Log Channels
     |--------------------------------------------------------------------------
@@ -48,76 +45,69 @@ return [
     |                    "errorlog", "monolog",
     |                    "custom", "stack"
     |
-    */
-
-    'channels' => [
-        'stack' => [
-            'driver' => 'stack',
-            'channels' => ['single'],
-            'ignore_exceptions' => false,
-        ],
-
-        'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-        ],
-
-        'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
-        ],
-
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => env('LOG_LEVEL', 'critical'),
-        ],
-
-        'papertrail' => [
-            'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
-            ],
-        ],
-
-        'stderr' => [
-            'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
-            'with' => [
-                'stream' => 'php://stderr',
-            ],
-        ],
-
-        'syslog' => [
-            'driver' => 'syslog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'facility' => LOG_USER,
-        ],
-
-        'errorlog' => [
-            'driver' => 'errorlog',
-            'level' => env('LOG_LEVEL', 'debug'),
-        ],
-
-        'null' => [
-            'driver' => 'monolog',
-            'handler' => NullHandler::class,
-        ],
-
-        'emergency' => [
-            'path' => storage_path('logs/laravel.log'),
-        ],
+   */
+  'channels' => [
+    'stack' => [
+      'driver' => 'stack',
+      'channels' => ['single'],
+      'ignore_exceptions' => false
     ],
-
+    'single' => [
+      'driver' => 'single',
+      'path' => storage_path('logs/laravel.log'),
+      'level' => env('LOG_LEVEL', 'debug')
+    ],
+    'daily' => [
+      'driver' => 'daily',
+      'path' => storage_path('logs/laravel.log'),
+      'level' => env('LOG_LEVEL', 'debug'),
+      'days' => 14
+    ],
+    'slack' => [
+      'driver' => 'slack',
+      'url' => env('LOG_SLACK_WEBHOOK_URL'),
+      'username' => 'Laravel Log',
+      'emoji' => ':boom:',
+      'level' => env('LOG_LEVEL', 'critical')
+    ],
+    'papertrail' => [
+      'driver' => 'monolog',
+      'level' => env('LOG_LEVEL', 'debug'),
+      'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
+      'handler_with' => [
+        'host' => env('PAPERTRAIL_URL'),
+        'port' => env('PAPERTRAIL_PORT'),
+        'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT')
+      ]
+    ],
+    'stderr' => [
+      'driver' => 'monolog',
+      'level' => env('LOG_LEVEL', 'debug'),
+      'handler' => StreamHandler::class,
+      'formatter' => env('LOG_STDERR_FORMATTER'),
+      'with' => [
+        'stream' => 'php://stderr'
+      ]
+    ],
+    'syslog' => [
+      'driver' => 'syslog',
+      'level' => env('LOG_LEVEL', 'debug'),
+      'facility' => LOG_USER
+    ],
+    'errorlog' => [
+      'driver' => 'errorlog',
+      'level' => env('LOG_LEVEL', 'debug')
+    ],
+    'null' => [
+      'driver' => 'monolog',
+      'handler' => NullHandler::class
+    ],
+    'emergency' => [
+      'path' => storage_path('logs/laravel.log')
+    ],
+    'deprecation' => [
+      'driver' => 'single',
+      'path' => storage_path('logs/deprecation_warnings.log')
+    ]
+  ]
 ];
